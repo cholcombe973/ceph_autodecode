db0b6b10a8cefe63e34ef4fc68c534b51fe6795f
Chris Holcombe <xfactor973@gmail.com>
2015-12-08 11:01:00 -0800

    Producing somewhat useful code now

--- decode.py
+++ decode.py
@@ -20,7 +20,10 @@ decoded_types = {
     'PushReplyOp': 'PushReplyOp',
     'PushOp': 'PushOp',
     'op_type_t': 'OpTypeT',
+    'pg_log_t': 'PgLogT',
+    'entity_name_t': 'EntityNameT',
     'entity_addr_t': 'EntityAddr',
+    'pg_info_t': 'PgInfoT',
 }
 
 write_to_wire_mapping = {
@@ -74,8 +77,9 @@ struct_mapping = {
     'version_t': 'u64',
     'ceph_tid_t': 'u64',
     'epoch_t': 'u32',
-    'std::string': '\'a &str',
-    'string': '\'a &str',
+    'object_t': '&\'a str',
+    'std::string': '&\'a str',
+    'string': '&\'a str',
 }
 
 nom_mapping = {
@@ -103,6 +107,7 @@ nom_mapping = {
     'version_t': 'le_u64',
     'ceph_tid_t': 'le_u64',
     'epoch_t': 'le_u32',
+    'object_t': 'parser_str',
     'std::string': 'parse_str',
     'string': 'parse_str',
 }
@@ -116,20 +121,32 @@ class RustStruct(object):
         self.members = members
 
 
-def add_unit_test(struct_name, parts):
-    output = ["#[test]",
-              "fn test_ceph_{}(){{".format(struct_name),
-              "let bytes = vec![", "];",
-              "let x: &[u8] = &[];",
-              "let expected_result = {} {{".format(struct_name),
-              "};",
-              "let result = {}::read_from_wire(&bytes);".format(struct_name),
-              "println!(\"ceph_connect_reply: {:?}\", result);",
-              "assert_eq!(Done(x, expected_result), result);",
+def add_read_unit_test(struct_name, members):
+    output = ["\n#[test]",
+              "fn test_ceph_read_{}(){{".format(struct_name),
+              "\tlet bytes = vec![",
+              "\t\t//TODO: fill in test data here",
+              "\t];",
+              "\tlet x: &[u8] = &[];",
+              "\tlet expected_result = {} {{".format(underscore_to_camelcase(struct_name)),
+              "\t};",
+              "\tlet result = {}::read_from_wire(&bytes);".format(underscore_to_camelcase(struct_name)),
+              "\tprintln!(\"ceph_connect_reply: {:?}\", result);",
+              "\tassert_eq!(Done(x, expected_result), result);",
               "}"]
     return output
 
-
+def add_write_unit_test(struct_name, members):
+    output = ["\n#[test]",
+              "fn test_ceph_write_{}(){{".format(underscore_to_camelcase(struct_name)),
+              "\tlet expected_bytes = vec![",
+              "\t\t//TODO: fill in result data here",
+              "\t];",
+              "\tlet result = {}::write_to_wire();".format(underscore_to_camelcase(struct_name)),
+              "\tprintln!(\"ceph_write_{}{{:?}}\", result);".format(underscore_to_camelcase(struct_name)),
+              "\t//assert_eq!(result, expected_bytes);",
+              "}"]
+    return output
 def resolve_type(ceph_type):
     print("Unknown type: " + ceph_type)
     output = []
@@ -172,7 +189,7 @@ def split_vector(ceph_vector):
 
 def add_struct(struct_name, public_properties):
     output = [
-        "#[derive(Debug,Eq,PartialEq)]",
+        "\n#[derive(Debug,Eq,PartialEq)]",
         "pub struct " + underscore_to_camelcase(struct_name) + "{"
     ]
     members = {}
@@ -188,17 +205,15 @@ def add_struct(struct_name, public_properties):
             if line['name'] in decoded_types:
                 output.append("\tpub {}: {},".format(line['name'], decoded_types[line['name']]))
             else:
-                pass
+                output.append("\tpub {}: {},".format(line['name'], line['name']))
                 # output.append("\tpub {}: {},".format(line['name'], line['type']))
                 # members[line['name']] = line['type']
     output.append("}")
-    decoded = RustStruct(name=struct_name, members=members)
-    # decoded_types.(decoded)
     return output
 
 
 def add_impl(struct_name, public_properties):
-    output = ["impl<'a> CephPrimitive<'a> for {}{{".format(underscore_to_camelcase(struct_name)),
+    output = ["\nimpl<'a> CephPrimitive<'a> for {}{{".format(underscore_to_camelcase(struct_name)),
               "\tfn read_from_wire(input: &'a [u8]) -> nom::IResult<&[u8], Self>{"]
     for i in public_properties:
         if i['name'] == 'HEAD_VERSION':
@@ -206,9 +221,11 @@ def add_impl(struct_name, public_properties):
             pass
         elif i['name'] == 'COMPAT_VERSION':
             output.append('\tlet compat_version = ' + str(i['static']) + ";")
+        elif i['type'].startswith('static const int'):
+            output.append('\tlet {} = {};'.format(i['name'], str(i['static'])))
+
     output.append("\tchain!(input,")
     for line in public_properties:
-        # print "line: " + str(line)
         ceph_type = line['type']
         if ceph_type == 'HEAD_VERSION':
             continue
@@ -218,71 +235,93 @@ def add_impl(struct_name, public_properties):
         if ceph_type.startswith('map'):
             ceph_type_map = split_map(ceph_type)
             if ceph_type_map[0] in decoded_types and ceph_type_map[1] in decoded_types:
-                output.append("\t{}: HashMap<{},{}>,".format(
+                output.append("\t\t{}: HashMap<{},{}>,".format(
                     line['name'],
                     decoded_types[ceph_type_map[0]],
                     decoded_types[ceph_type_map[1]],
                 ))
             else:
-                pass
+                # Fix this manually
+                output.append("\t\t{}: HashMap<{},{}>,".format(
+                    line['name'],
+                    ceph_type_map[0],
+                    ceph_type_map[1],
+                ))
+        # Skip constants, we defined them above
+        elif ceph_type.startswith('static const int'):
+            continue
+        elif ceph_type.startswith('const static int'):
+            continue
 
         elif ceph_type.startswith('pair'):
             ceph_type_map = split_map(ceph_type)
             if ceph_type_map[0] in decoded_types:
-                output.append("\t{}: HashMap<{},{}>,".format(
+                output.append("\t\t{}: pair!({},{}),".format(
                     line['name'],
                     decoded_types[ceph_type_map[0]],
                     decoded_types[ceph_type_map[1]]
                 ))
             else:
-                pass
+                # Fix this manually
+                output.append("\t\t{}: pair!(call!({}::read_from_wire),call!({}::read_from_wire)),".format(
+                    line['name'],
+                    ceph_type_map[0],
+                    ceph_type_map[1]
+                ))
         elif ceph_type.startswith('std::map'):
             ceph_type_map = split_map(ceph_type)
             if ceph_type_map[0] in decoded_types:
-                output.append("\t{}: HashMap<{},{}>,".format(
+                output.append("\t\t{}: HashMap<{},{}>,".format(
                     line['name'],
                     decoded_types[ceph_type_map[0]],
                     decoded_types[ceph_type_map[1]]
                 ))
             else:
-                pass
+                # Fix this manually
+                output.append("\t\t{}: HashMap<{},{}>,".format(
+                    line['name'],
+                    ceph_type_map[0],
+                    ceph_type_map[1]
+                ))
 
         elif ceph_type.startswith('vector'):
             ceph_type_vector = split_vector(ceph_type)
             if ceph_type_vector in nom_mapping:
-                output.append("\tcount: le_u32 ~")
-                output.append("\t{}: count!({}, count),".format(line['name'], nom_mapping[ceph_type_vector]))
+                output.append("\t\tcount: le_u32 ~")
+                output.append("\t\t{}: count!({}, count),".format(line['name'], nom_mapping[ceph_type_vector]))
             else:
-                pass
-                # print("Unknown vector type: " + str(ceph_type))
+                # We'll have to fix this manually
+                output.append("\t\tcount: le_u32 ~")
+                output.append("\t\t{}: count!(call!({}::read_from_wire), count),".format(line['name'], ceph_type_vector))
 
         elif ceph_type.startswith('list') or ceph_type.startswith('set'):
             ceph_type_vector = split_vector(ceph_type)
             if ceph_type_vector in nom_mapping:
-                output.append("\tcount: le_u32 ~")
-                output.append("\t{}: count!({},count)".format(line['name'], nom_mapping[ceph_type_vector]))
+                output.append("\t\tcount: le_u32 ~")
+                output.append("\t\t{}: count!({},count)".format(line['name'], nom_mapping[ceph_type_vector]))
             else:
-                pass
-                # print("Unknown list type: " + str(ceph_type))
+                # We'll have to fix this manually
+                output.append("\tcount: le_u32 ~")
+                output.append("\t{}: count!(call!({}::read_from_wire),count)".format(line['name'], ceph_type_vector))
 
         elif ceph_type in nom_mapping:
             output.append("\t\t" + line['name'] + ": " + nom_mapping[ceph_type] + " ~")
         else:
             if ceph_type in decoded_types:
-                output.append("\t{}: {},".format(line['name'], decoded_types[ceph_type]))
+                output.append("\t\t{}: {} ~".format(line['name'], decoded_types[ceph_type]))
             else:
-                output.append("\t{}: {},".format(line['name'], ceph_type))
+                # We'll have to fix this manually
+                output.append("\t\t{}: call!({}::read_from_wire) ~".format(line['name'], ceph_type))
                 print("Unknown type line 207: '" + str(ceph_type) + "'")
-                #pass
-        # resolved_parts = resolve_type(ceph_type=ceph_type)
-        #    for resolved_part in resolved_parts:
-        #        output.append(resolved_part + ": " + nom_mapping[resolved_part] + "~")
+                # resolved_parts = resolve_type(ceph_type=ceph_type)
+                #    for resolved_part in resolved_parts:
+                #        output.append(resolved_part + ": " + nom_mapping[resolved_part] + "~")
     output.append("\t\t||{")
     output.append("\t\t\t" + underscore_to_camelcase(struct_name) + "{")
     for line in public_properties:
-        if line['name'] == 'HEAD_VERSION':
+        if line['name'].startswith('static const int'):
             continue
-        elif line['name'] == 'COMPAT_VERSION':
+        elif line['name'].startswith('const static int'):
             continue
         output.append("\t\t\t" + line['name'] + ": " + line['name'] + ",")
     output.append("\t\t}")
@@ -293,6 +332,8 @@ def add_impl(struct_name, public_properties):
     output.append("\t\treturn Ok(buffer);")
     output.append("\t}")
     output.append("}")
+    decoded_types[struct_name] = underscore_to_camelcase(struct_name)
+
     return output
 
 
@@ -302,17 +343,19 @@ def decode_ceph_messages():
         "src/osd/ECMsgTypes.h",
         "src/osd/osd_types.h",
         'src/osd/OpRequest.h',
+        'src/msg/msg_types.h',
     ]
     headers = os.listdir(os.path.join(prefix, 'src/messages'))
     for header in headers:
         try:
-            #print("parsing " + header)
             cpp_header = CppHeaderParser.CppHeader(os.path.join(prefix, 'src/messages', header))
             try:
                 for clazz in cpp_header.classes:
                     parts = cpp_header.classes[clazz]['properties']['public']
-                    #print("Decoding: " + str(clazz))
-                    add_unit_test(struct_name=clazz, parts=parts)
+                    for output in add_read_unit_test(struct_name=clazz, members=parts):
+                        print output
+                    for output in add_write_unit_test(struct_name=clazz, members=parts):
+                        print output
                     for output in add_struct(struct_name=clazz, public_properties=parts):
                         print output
                     for output in add_impl(struct_name=clazz, public_properties=parts):
@@ -325,13 +368,14 @@ def decode_ceph_messages():
     # Parse the extra headers to get everything
     for extra in extras:
         try:
-            print("parsing extra: " + extra)
             cpp_header = CppHeaderParser.CppHeader(os.path.join(prefix, extra))
             try:
                 for clazz in cpp_header.classes:
                     parts = cpp_header.classes[clazz]['properties']['public']
-                    #print("Decoding: " + str(clazz))
-                    for output in add_unit_test(struct_name=clazz, parts=parts):
+                    # print("Decoding: " + str(clazz))
+                    for output in add_read_unit_test(struct_name=clazz, members=parts):
+                        print output
+                    for output in add_write_unit_test(struct_name=clazz, members=parts):
                         print output
                     for output in add_struct(struct_name=clazz, public_properties=parts):
                         print output
